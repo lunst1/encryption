1083158dab8ee4ce35f44bbbe23acd947caf8990
LunST <lunststunt@gmail.com>
2021-03-20 03:58:43 +0300

    Case with SHA2

--- encryption.rb
+++ encryption.rb
@@ -2,13 +2,18 @@ require 'digest'
 puts "Напишите ваше сообщение:"
 a = gets.chomp
 puts "Выберете метод шифрования:
-1. MD5
-2. SHA1"
+1. Md5
+2. Sha1
+3. Sha2"
 b = gets.chomp.to_i
-if b == 1
-  puts "Вот что получилось:"
-  puts Digest::MD5.hexdigest a
-else
-  puts "Вот что получилось:"
-  puts Digest::SHA1.hexdigest a
+case b
+  when 1
+    puts "Вот что получилось:"
+    puts Digest::MD5.hexdigest a
+  when 2
+    puts "Вот что получилось:"
+    puts Digest::SHA1.hexdigest a
+  when 3
+    puts "Вот что получилось:"
+    puts Digest::SHA2.hexdigest a
 end
