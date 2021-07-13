3faf6f9d7995e5315e49bfa1091427f60a17b4e8
Alexandr <44715875+lunst97@users.noreply.github.com>
2021-07-13 13:08:56 +0300

    Refactor codestyle

--- encryption.rb
+++ encryption.rb
@@ -1,4 +1,5 @@
 require 'digest'
+
 puts "Напишите ваше сообщение:"
 a = gets.chomp
 puts "Выберете метод шифрования:
@@ -6,14 +7,15 @@ puts "Выберете метод шифрования:
 2. Sha1
 3. Sha2"
 b = gets.chomp.to_i
+
 case b
   when 1
-    puts "Вот что получилось:"
+    puts "Зашифрованное сообщение:"
     puts Digest::MD5.hexdigest a
   when 2
-    puts "Вот что получилось:"
+    puts "Зашифрованное сообщение:"
     puts Digest::SHA1.hexdigest a
   when 3
-    puts "Вот что получилось:"
+    puts "Зашифрованное сообщение:"
     puts Digest::SHA2.hexdigest a
 end
